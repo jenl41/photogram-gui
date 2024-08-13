e1a9d52f1f8e6e37563d96ceb8789962e71026d6
jenl41 <lee.jennifer@chicagobooth.edu>
2024-08-13 06:02:31 +0000

    add photos

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,11 @@
 class PhotosController < ApplicationController
 
   def index
-render({ :template => "photo_templates", :action => "index"})
+
+matching_photos = Photo.all
+@list_of_photos = matching_photos.order({ :created_at => :desc })
+
+    render ({:template => "photo_templates/index"})
   end
 
 
