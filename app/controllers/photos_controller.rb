8e08fe514d316c5d167d413612b259b0407c6359
jenl41 <lee.jennifer@chicagobooth.edu>
2024-08-13 06:34:44 +0000

    add create and delete

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -9,4 +9,47 @@ matching_photos = Photo.all
   end
 
 
+  def show
+
+    url_id = params.fetch("path_id")
+    matching_photos = Photo.where({:id => url_id})
+
+    @the_photo = matching_photos.at(0)
+
+    render ({:template => "photo_templates/details"})
+  end
+
+def baii
+the_id = params.fetch("toast_id")
+matching_photos = Photo.where({:id =>the_id})
+the_photo = matching_photos.at(0)
+the_photo.destroy
+
+the_photo
+
+  #render({:template => "photo_templates/baii"})
+
+  redirect_to("/photos")
+end
+
+def create
+input_image = params.fetch("query_image")
+input_caption = params.fetch("query_caption")
+input_owner_id = params.fetch("query_owner_id")
+
+a_new_photo = Photo.new
+
+a_new_photo.image = input_image
+a_new_photo.caption = input_caption
+a_new_photo.owner_id = input_owner_id
+
+a_new_photo.save
+
+ # render({:template => "photo_templates/create"})
+
+ redirect_to("/photos/ + a_new_photo.id.to_s")
+end
+
+
+
 end
